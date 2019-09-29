17aa4602acc4fe8fc06ce2fe62bba531284deb96
Abdul Rehman <mehars.6925@gmail.com>
2019-09-29 05:38:33 -0700

    logout view

---
+++ src/views/auth/Logout.vue
@@ -0,0 +1,17 @@
+<template>
+    <div></div>
+</template>
+
+<script>
+export default {
+    beforeRouteEnter(to, from, next) {
+        next(vm => {
+            if (vm.$root.isLoggedIn){
+                vm.$root.logout();
+                vm.$toastr('info', 'You\'re logged out successfully.', 'Information')
+                vm.$router.go(-1)
+            }
+        })
+    }
+}
+</script>
