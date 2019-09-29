0655512cb59e273b8d8ded499fa55665456615da
Abdul Rehman <mehars.6925@gmail.com>
2019-09-29 05:36:04 -0700

    preloader

---
+++ src/components/Preloader.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="prelaoder">
+        <h1 class="title is-3">{{ $root.name }}</h1>
+        <br />
+        <div class="loader-4 center"><span></span></div>
+        <span>Loading....</span>
+    </div>
+</template>
+
+<script>
+export default {
+    // name: 'prelaoder'
+}
+</script>
+
+<style src="../assets/css/preloader.css"></style>
