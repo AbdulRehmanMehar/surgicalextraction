27297cf4d3baf1dbbf9ac9ef78d4adf9fe002d6d
Abdul Rehman <mehars.6925@gmail.com>
2019-09-29 08:08:23 -0700

    init vuex

---
+++ src/vuex/index.js
@@ -0,0 +1,13 @@
+import cart from './modules/cart'
+import auth from './modules/auth'
+import product from './modules/product'
+import category from './modules/category'
+
+export default {
+    modules: {
+        cart,
+        auth,
+        product,
+        category
+    }
+}
