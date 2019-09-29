071c2237bb444c2d46508159469079de8387b5de
Abdul Rehman <mehars.6925@gmail.com>
2019-09-29 05:28:49 -0700

    surgical extraction application

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,71 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <Preloader :class="{'hidden': !$root.loading}" />
+        <div>
+            <Navigation :categories="categories" v-if="$root.navbar" />
+            <router-view></router-view>
+            <Foot v-if="$root.footer" />
+        </div>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+const Foot = () => import('./components/Foot')
+const Preloader = () => import('./components/Preloader')
+const Navigation = () => import('./components/Navigation')
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
+    name: 'app',
+    data() {
+        return {
+            categories: null
+        }
+    },
+    created: function () {
+        this.loadCategories()
+    },
+    methods: {
+        loadCategories: function () {
+            this.$store.dispatch('get_all_categories')
+            .then(resp => {
+                this.categories = resp.data.data
+            })
+            .catch(error => console.log(error))
+        },
+    },
+    components: {
+        Foot,
+        Preloader,
+        Navigation,
+    }
+
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+:root {
+    --app-color: #144B8E;
+    --app-text-color: #C0D6DF;
+    --app-accent-color: #589694;
+    --app-text-hover-color: #FF8552;
+    --app-text-accent-color: #E6E6E6;
+}
+.columns {
+    margin-left: 0px;
+    margin-right: 0px;
+}
+.hidden {
+    display: none;
+}
+.VueCarousel-slide.image {
+    background-color: #ccc;
+}
+
+.disabled {
+    color: #7a7a7a;
+    cursor: not-allowed;
+    pointer-events: none;
+    border-color: #dbdbdb;
+    background-color: #dbdbdb;
 }
 </style>
