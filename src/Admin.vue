6dd398ec7e4380ae720cd593627f5a8c641700bc
Abdul Rehman <mehars.6925@gmail.com>
2019-09-29 05:27:59 -0700

    admin middleware

---
+++ src/Admin.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <router-view></router-view>
+    </div>
+</template>
+
+<script>
+export default {
+    beforeRouteEnter(to, from, next) {
+        next(vm => {
+            if (vm.$root.currentUser.role != 'admin') {
+                vm.$toastr('danger', 'You\'re not authorized.', 'Aaah!')
+                vm.$router.push('/')
+            }
+        })
+    },
+    created() {
+        if (this.$root.currentUser.role != 'admin') {
+                this.$toastr('danger', 'You\'re not authorized.', 'Aaah!')
+                this.$router.push('/')
+            }
+    }
+}
+</script>
