10dade2bb3081d4bb33b79a17d06f05b7e1957cf
Abdul Rehman <mehars.6925@gmail.com>
2019-09-29 05:36:30 -0700

    cart quantity update module

---
+++ src/components/QuantityUpdateModule.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="field has-addons">
+        <div class="control">
+            <input class="input" type="text" v-model="quantity" style="min-width: 100px">
+        </div>
+        <div class="control">
+            <a class="button is-info" @click.prevent="$root.addToCart($event, product, quantity )">
+                Update
+            </a>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['cart'],
+    data() {
+        return {
+            quantity: 10,
+            product: null,
+        }
+    },
+    created() {
+        this.quantity = this.$props.cart.quantity
+        this.product = this.$props.cart.product
+    }
+}
+</script>
