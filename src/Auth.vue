d91406e03ad483de6a4708359965bb708502e715
Abdul Rehman <mehars.6925@gmail.com>
2019-09-29 05:28:25 -0700

    authentication middleware

---
+++ src/Auth.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <router-view></router-view>
+    </div>
+</template>
+
+<script>
+export default {
+    beforeRouteEnter(to, from, next) {
+        next(vm => {
+            if (!vm.$root.isLoggedIn) {
+                vm.$toastr('danger', 'You\'re not logged in.', 'Aaah!')
+                vm.$router.push('/')
+            }
+        })
+    },
+    created() {
+        if (!this.$root.isLoggedIn) {
+                this.$toastr('danger', 'You\'re not logged in.', 'Aaah!')
+                this.$router.push('/')
+            }
+    }
+}
+</script>
